c575a7a56b9bd26fe65e462378905f06c85959d4
Hee-Tae Shin <greenux@naver.com>
2017-11-25 18:58:04 +0900

    Some remarks are added

--- Config.mk
+++ Config.mk
@@ -6,7 +6,7 @@ DEFINES	:= -DDEBUG
 CFLAGS	:= -O2 -W -Wall $(INCLUDES) $(DEFINES)
 AR_MAKE_FLAGS := -rscv
 AR_ADDING_FLAGS := -rvcT
-.SUFFIXES: .o .c .S .a
+.SUFFIXES: .o .c .S .a		#Element of .SUFFIXES are used to check extention rules. #.SUFFIXES요소들은 확장자 규칙을 검사하는데 사용된다.
 
 %.o:%.c
 	$(CC) $(CFLAGS) -c $< -o $@
